1f3025f52db8eb4f4217a1d17fd554b7db8f96df
Hidelbrand <hidelbrandosantos3@gmail.com>
2023-02-02 11:08:30 -0300

    textaArea

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './ChatWindow.css'
+import React, {useState} from 'react';
+import './ChatWindow.css';
+import EmojiPicker from 'emoji-picker-react';
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -7,50 +8,135 @@ import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import CloseIcon from '@mui/icons-material/Close';
 import SendIcon from '@mui/icons-material/Send';
 import MicIcon from '@mui/icons-material/Mic';
+import MessageItem from './MessageItem';
 
 
 
-const ChatWindow = ({data}) => {
+const ChatWindow = () => {
+
+    let recognition = null;
+    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if(SpeechRecognition !== undefined) {
+        recognition = new SpeechRecognition();
+    }
+
+    const [emojiOpen, setEmojiOpen] = useState(false);
+    const [text, setText] = useState('');
+    const [listening, setListening] = useState(false);
+    const [list, setList] = useState([{}, {}, {}]);
+
+    const handleEmojiclick = (e, emojiObject) => {
+        setText(text + emojiObject.emoji)
+    }
+
+    const handleOpenEmoji = () => {
+        setEmojiOpen(true)
+    }
+
+    const handleCloseEmoji = () => {
+        setEmojiOpen(false)
+    }
+
+    const handleMicClick = () => {
+        if(recognition !== null) {
+            recognition.onstart = () => {
+                setListening(true)
+            }
+            recognition.onend = () => {
+                setListening(false)
+            }
+            recognition.onresult = (e) => {
+                setText(e.results[0][0].transcript)
+            }
+            recognition.start();
+        }
+    }
+
+    const handleSendClick = () => {
+
+    }
+
   return (
-    <div className='chatWindow'>
-        <div className='chatWindowHeader'>
-            <div className='headerInfo'>
-                <img className='avatar' src='https://www.w3schools.com/howto/img_avatar.png'/>
-                <div className='name'>Nana</div>
-            </div>
+    <div className="chatWindow">
+      <div className="chatWindowHeader">
+        <div className="headerInfo">
+          <img
+            className="avatar"
+            src="https://www.w3schools.com/howto/img_avatar.png"
+          />
+          <div className="name">Nana</div>
+        </div>
 
-            <div className='header-buttons'>
-                <div className='chat-btn'>
-                    <SearchIcon style={{color: '#919191'}}/>
-                </div>
-                <div className='chat-btn'>
-                    <AttachFileIcon style={{color: '#919191'}}/>
-                </div>
-                <div className='chat-btn'>
-                    <MoreVertIcon style={{color: '#919191'}}/>
-                </div>
-            </div>
+        <div className="header-buttons">
+          <div className="chat-btn">
+            <SearchIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chat-btn">
+            <AttachFileIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chat-btn">
+            <MoreVertIcon style={{ color: "#919191" }} />
+          </div>
         </div>
-        <div className='chatWindowBody'>
+      </div>
+      <div className="chatWindowBody">
+        {list.map((item, key)=>(
+            <MessageItem 
+                key={key}
+                data={item}
+            />
+        ))}
+      </div>
+      <div
+        className="emojiArea"
+        style={{ height: emojiOpen ? "200px" : "0px" }}
+      >
+        <EmojiPicker
+          onEmojiClick={handleEmojiclick}
+          disableSearchBar
+          disableSkinTonePicker
+        />
+      </div>
+      <div className="chatWindowFooter">
+        <div className="pre">
+          <div
+            className="chat-btn"
+            onClick={handleCloseEmoji}
+            style={{ width: emojiOpen ? 40 : 0 }}
+          >
+            <CloseIcon style={{ color: "#919191" }} />
+          </div>
+          <div className="chat-btn" onClick={handleOpenEmoji}>
+            <InsertEmoticonIcon
+              style={{ color: emojiOpen ? "#009688" : "#919191" }}
+            />
+          </div>
+        </div>
+        <div className="input-area">
+          <input
+            className="input"
+            type="text"
+            placeholder="Digite uma mensagem"
+            value={text}
+            onChange={e=>setText(e.target.value)}
+          />
+        </div>
 
-        </div>
-        <div className='chatWindowFooter'>
-            <div className='pre'>
-                <div className='chat-btn'>
-                    <InsertEmoticonIcon style={{color: '#919191'}}/>
+        <div className="pos">
+            {text === '' && 
+                <div onClick={handleMicClick} className="chat-btn">
+                    <MicIcon style={{ color: listening? '#126ece' : "#919191" }} />
                 </div>
-            </div>
-            <div className='input-area'>
-                <input className='input' type='text' placeholder='Digite uma mensagem'/>
-            </div>
-            <div className='pos'>
-            <div className='chat-btn'>
-                    <SendIcon style={{color: '#919191'}}/>
+            }
+            {text !== '' && 
+                <div onClick={handleSendClick} className="chat-btn">
+                    <SendIcon style={{ color: "#919191" }} />
                 </div>
-            </div>
+            }
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ChatWindow
---
+++ src/components/MessageItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MessageItem.css'
+
+const MessageItem = () => {
+  return (
+    <div className='messageLine'>
+      <div className='messageItem'>
+        <div className='messageText'>ContentArea (Exibindo mensagem no chat)
+        ContentArea (Exibindo mensagem no chat)
+</div>
+        <div className='messageDate'>19:52</div>
+      </div>
+    </div>
+  )
+}
+
+export default MessageItem
